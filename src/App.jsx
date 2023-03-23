76fa4c75be0e930f2ccf0c491a38a2e4f92bb0bd
XKingPoseidonX <charlyhdez2001@gmail.com>
2023-03-23 09:21:14 -0600

    parte carlos

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,30 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import { Button, Stack, Nav, Container, Navbar } from 'react-bootstrap';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <>
+      <Navbar bg="dark" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">Pick a Player</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">INICIO </Nav.Link>
+            <Nav.Link href="#clubes">LUBES </Nav.Link>
+            <Nav.Link href="#jugadores">UGADORES </Nav.Link>
+            <Nav.Link href="#predicciones">REDICCIONES </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    </>
   )
 }
 
